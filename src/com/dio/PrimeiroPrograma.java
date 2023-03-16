bf17d96ace2e64178d07b97c16b77a06c38fc810
JucieleGomes <jucielegomes.s@hotmail.com>
2023-03-16 16:42:27 -0300

    First commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livro();
+		
+		System.out.println(gato);
+    
+	 /*int a = 2;
+     int b = 3;
+     System.out.println("Hello Word! " + (a+b));*/
+	}
+
+}
+
+class livros {
+	private String nome;
+	private String npag;
+	
+}
